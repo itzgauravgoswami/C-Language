c705a8c2cf71df7bd00396732300695de064453c
Gaurav Goswami <142787056+itzgauravgoswami@users.noreply.github.com>
2023-10-30 18:12:08 +0530

    Delete Day 9 directory

--- Day 9/index.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    for (int i = 1; i <= 10; i+=1)
-    {
-        printf("Hello World\n");
-    }
-    
-}
--- Day 9/index2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int i=1;
-    int j=i++;
-    int k=++j;
-    printf(i,j,k);
-}
